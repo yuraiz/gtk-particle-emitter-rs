344eb3bd575244b33f6d20f045aeeeb2d14ac847
YuraIz <7516890@gmail.com>
2023-03-26 13:07:47 +0300

    gtk-emitter: Don't draw items those will be cropped

--- gtk-particle-emitter/src/emitter.rs
+++ gtk-particle-emitter/src/emitter.rs
@@ -35,6 +35,10 @@ mod imp {
 
     impl WidgetImpl for ParticleEmitter {
         fn snapshot(&self, snapshot: &gtk::Snapshot) {
+            let widget = self.obj();
+            let width = widget.width() as f32;
+            let height = widget.height() as f32;
+
             let ticker = self.ticker.borrow();
             let particles = ticker.particles();
 
@@ -44,15 +48,6 @@ mod imp {
             }
 
             particles.for_each(|particle| {
-                let mut color_matrix = [0.0; 16];
-                color_matrix[15] = particle.alpha();
-                let color_matrix = { graphene::Matrix::from_float(color_matrix) };
-
-                let c = particle.color();
-                let color_offset = { graphene::Vec4::from_float([c.r, c.g, c.b, 0.0]) };
-
-                snapshot.push_color_matrix(&color_matrix, &color_offset);
-
                 let (image, rect) = {
                     let particle = &particle;
                     let (x, y) = particle.coords();
@@ -71,6 +66,25 @@ mod imp {
                     (image, rect)
                 };
 
+                // no need to draw items those will be cropped
+                if widget.overflow() == gtk::Overflow::Hidden {
+                    let br = rect.bottom_right();
+                    let tl = rect.top_left();
+
+                    if br.x() <= 0.0 || br.y() <= 0.0 || tl.x() >= width || tl.y() >= height {
+                        return;
+                    }
+                }
+
+                let mut color_matrix = [0.0; 16];
+                color_matrix[15] = particle.alpha();
+                let color_matrix = { graphene::Matrix::from_float(color_matrix) };
+
+                let c = particle.color();
+                let color_offset = { graphene::Vec4::from_float([c.r, c.g, c.b, 0.0]) };
+
+                snapshot.push_color_matrix(&color_matrix, &color_offset);
+
                 snapshot.append_texture(image, &rect);
                 snapshot.pop();
             });
@@ -87,6 +101,7 @@ glib::wrapper! {
 impl ParticleEmitter {
     pub fn new(config: Config, textures: Vec<gdk::Texture>) -> Self {
         let obj: Self = glib::Object::new();
+        obj.set_overflow(gtk::Overflow::Hidden);
         obj.imp().ticker.replace(Ticker::with_config(config));
         obj.imp().textures.replace(textures);
         obj
